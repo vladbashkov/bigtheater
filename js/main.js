9d18c4c444da83a9b0799082aa22f50705e1bb96
vladbashkov <vladbashkv1@gmail.com>
2021-11-08 11:52:20 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,46 @@
-const navMenu = document.querySelector(".nav-menu");
+const body = document.querySelector('body'),
+   navMenu = document.querySelector('.nav-menu'),
+   navBar = document.querySelector('.navbar'),
+   contIcons = document.querySelector('.contacts-icons'),
+   phoneNumber = document.querySelector('.phone-number'),
+   menuBtnOpen = document.querySelector('.menu-btn'),
+   menuBtnClose = document.querySelector('.menu-close');
 
 window.onscroll = function () {
     if(scrollY > 100) {
-        navMenu.classList.add("navBackground")
+        navMenu.classList.add('navBackground');
+        navBar.classList.add('navbarPadding');
+        phoneNumber.classList.add('opacity');
     }
     else if(scrollY < 100){
-        navMenu.classList.remove("navBackground")
+        navMenu.classList.remove("navBackground");
+        navBar.classList.remove('navbarPadding');
+        phoneNumber.classList.remove('opacity');
     }
 }
+
+let menuOpen = false;
+menuBtnOpen.addEventListener('click', ()=>{
+   if(!menuOpen){
+      navBar.classList.add('show');
+      contIcons.classList.add('show-icons');
+      phoneNumber.classList.add('opacity');
+      body.classList.add('ofhd');
+      menuOpen = true;
+   }
+})
+
+menuBtnClose.addEventListener('click', ()=>{
+   if(menuOpen){
+      navBar.classList.remove('show');
+      contIcons.classList.remove('show-icons');
+      phoneNumber.classList.remove('opacity');
+      body.classList.remove('ofhd');
+      menuOpen = false;
+   } 
+})
+
+
 
 new Swiper('.swiper', {
     direction: 'horizontal',
@@ -27,7 +60,7 @@ new Swiper('.projects-swiper', {
     slidesPerView: 4,
     freeMode: true,
     spaceBetween: 0,
-    simulateTouch:true,
+    simulateTouch: true,
     breakpoints: {
         1920: {
            slidesPerView: 4
@@ -63,7 +96,7 @@ new Swiper('.projects-swiper', {
            slidesPerView: 2
         },
         950: {
-           slidesPerView: 1.9
+           slidesPerView: 1.9,
         },
         900: {
            slidesPerView: 1.8
@@ -97,7 +130,6 @@ new Swiper('.projects-swiper', {
 
 new Swiper('.partners-swiper', {
     direction: 'horizontal',
-    loop: true,
     slidesPerView: 4,
 
     grid: {
@@ -118,8 +150,14 @@ new Swiper('.partners-swiper', {
         576: {
             slidesPerView: 2
         },
+        425: {
+           slidesPerView: 1.5
+        },
         380: {
-            slidesPerView: 1
+            slidesPerView: 1.3
+        },
+        320: {
+           slidesPerView: 1.1
         },
     }
 });
